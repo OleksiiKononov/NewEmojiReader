826d4246377a0f13c371fbbe8b1d7fd323eb8d95
OleksiiKononov <kononov.oleksii@gmail.com>
2021-04-08 16:52:09 +0300

    connected TableViewController

---
+++ NewEmojiReader/NewEmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewEmojiReader
+//
+//  Created by SASMobile on 08.04.2021.
+//  Copyright © 2021 Oleksii Kononov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
