b69b2ff17d6af595c47f7e8b379bca1177fe3240
OleksiiKononov <kononov.oleksii@gmail.com>
2021-04-09 17:43:23 +0300

    add emoji cell

---
+++ NewEmojiReader/NewEmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  NewEmojiReader
+//
+//  Created by SASMobile on 09.04.2021.
+//  Copyright © 2021 Oleksii Kononov. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
